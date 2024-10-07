d17347fc8614ff81ee7f80ef8f2ef04fc9710b5b
ASUS <nethmidileksha6@gmail.com>
2024-10-07 22:25:24 +0530

    css problem solve

--- JS/navbar.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('mobile-menu');
-const dropdownMenu = document.getElementById('dropdown');
-
-menuToggle.addEventListener('click', function() {
-    dropdownMenu.classList.toggle('open');
-});
